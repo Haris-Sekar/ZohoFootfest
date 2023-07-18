332b8ff7e26df802d83b4ab5f9acdf08a7ee6410
Haris <sharis14003@gmail.com>
2023-07-18 16:51:09 +0530

    league page added

--- client/src/components/Routers.js
+++ client/src/components/Routers.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ChampionsLeague from "./Champions League/ChampionsLeague";
 import Home from "./Home";
+import League from "./League/League";
 const Routers = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/championsLeague/*" element={<ChampionsLeague />} />
+          <Route path="/league/*" element={<League />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
